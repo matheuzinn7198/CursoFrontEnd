cd57554ade5250b018964541be3b39b1ced4e07e
matheuzinn7198 <craquedebola07@gmail.com>
2025-02-25 11:08:57 -0300

    25.02.25

--- ArrayMatrix/script00.js
+++ ArrayMatrix/script00.js
@@ -40,3 +40,47 @@ console.log(arrayTexto);
 for(let i =0;i<arrayNumeros.length;i++){
     console.log("Índice["+i+"]="+[arrayNumeros[i]]);
 }
+
+//forEach
+arrayTexto.forEach(elemento => {
+    console.log(elemento)
+});
+
+//Métodos Úteis
+
+//IndexOf
+console.log(arrayNumeros.indexOf(5)); //4
+console.log(arrayNumeros.indexOf(10)); //-1 (elemento inexistente)
+
+//Splice (remover o elemento da posição)
+arrayMisto.splice(2,1); 
+console.log(arrayMisto);
+
+//Operações avaçadas de Arrays
+// map - novos valores
+let valores = [10, 20, 30, 40, 50];
+let valoresDobro = valores.map(x => x*2);
+console.log(valoresDobro);
+
+//filter - comparação
+let valoresFilter = valores.filter(x => x>25);
+console.log(valoresFilter);
+
+//desafio (filtro x<35) && (x*3) == [30, 60, 90]
+
+let valoresDesafio = [10, 20, 30];
+let valoresTriplo = valores.map(x => x*3);
+console.log(valoresTriplo);
+
+let valoresDesafioFilter = valores.filter(x => x<35);
+console.log(valoresFilter);
+
+//reduce ([] -> let x)
+let soma = valores.reduce((x,y) => x+y);
+console.log(soma);
+
+//sort (organizar/ordenado)
+let aleatorio = [7, 4, 2, 9, 1, 5, 8, 3, 6];
+console.log(aleatorio);
+let ordenado = aleatorio.sort();
+console.log(ordenado);
