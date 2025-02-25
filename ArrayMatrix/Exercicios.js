28f1e20ce99b3a4516f06603196ed10dd204db45
matheuzinn7198 <craquedebola07@gmail.com>
2025-02-25 11:26:05 -0300

    25.02.25

---
+++ ArrayMatrix/Exercicios.js
@@ -0,0 +1,13 @@
+//Lista de Exercicios
+//1. Criando e Manipulando Arrays
+
+let arrayFrutas = ["Maçã", "Banana", "Laranja", "Uva"]
+console.log(arrayFrutas["Banana"]);
+
+arrayFrutas.push("Melancia");
+console.log(arrayFrutas);
+
+arrayFrutas.shift();
+console.log(arrayFrutas);
+
+console.log(arrayTexto[1]);
