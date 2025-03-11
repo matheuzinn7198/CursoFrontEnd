15a03b73d8750d10e7a8e410da54a95a3653b655
matheuzinn7198 <craquedebola07@gmail.com>
2025-03-11 11:15:29 -0300

    11.03.25

---
+++ DOM/IntroducaoDOM/script.js
@@ -0,0 +1,45 @@
+//Script de Manipulação DOM
+
+//getElementById -> variavel
+let titulo = document.getElementById("titulo");//html -> id
+console.log(titulo); // mostra no console as informações da id
+titulo.innerText = "Outro Titulo"; // modificar o conteúdo da id
+
+//getElementByTagName -> variavel Vetor(Array)
+let paragrafo = document.getElementsByTagName("p"); //html -> "p"
+// modificar elemento
+paragrafo[0].innerText = "Exemplo de Parágrafo Modificado por DOM";
+// modificar style do elemento
+paragrafo[1].style.color = "red";
+
+//getElementsByClassName -> variavel Vetor(Array)
+let descricao = document.getElementsByClassName("descricao");
+for(let i = 0; i<descricao.length; i++){
+    descricao[i].style.color = "blue";
+}
+
+//querySelector -> variavel simples -> seleciona o 1°
+let p = document.querySelector("p");
+//modificação de font
+p.style.fontWeight = "bold";
+
+//querySelectorAll -> variavel Vetor(Array) -> Selecionar Todos
+let descricaoAll = document.querySelectorAll(".descricao");
+descricaoAll.forEach(element => {
+    element.style.fontWeight = "bold";
+});
+
+//eventos listener(ouvinte)
+
+//
+function MudarCorFundo(){
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "green";
+}
+
+//chamando direto no script o ouvinte
+document.getElementById("btnColor").addEventListener("click", OutraCor)
+function OutraCor(){
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "orange";
+}
