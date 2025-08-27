6de7fe84836b977c2859abb66b67b3e8625cce77
matheuzinn7189 <craquedebola07@gmail.com>
2025-08-27 16:05:52 -0300

    27.08.25

---
+++ React/hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!!!</h1>
+      <p>Minha Primeira Página React</p>
+    </div>
+  );
+}
+
+export default App;
